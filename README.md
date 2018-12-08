74b79cef41915e84150fb6015dd848f60d87ea18
Hans-Christian Psaar <shopfreelancer@gmail.com>
2018-12-08 08:46:39 +0100

    added moment.js to format and display dates in human friendly way added screenshot of app

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 Small project to make use of Graphql and Apollo Server as an alternative paradigm to define APIs.
 Uses a node.js / express backend and a vue.js frontend.
 
+
+
 Backend makes use of mongoose and MongoDB which could be set up at MongoDB Atlas, mlab or as a Docker container.
 
 ## Development
 Set your .env variables. Copy and rename nodemon.example.json to nodemon.json
 Insert connection to your MongoDB here.
 
+![Vue GraphQl Screenshot](./docs/vue_graphql.jpg?raw=true "GraphQl")
 
 Run the following command in root folder. This will start up both the node backend AND the vue frontend which is located in /client
 ```
--- client/src/components/Posts/Posts.vue
+++ client/src/components/Posts/Posts.vue
@@ -29,7 +29,7 @@
 
                 <v-list-tile-content>
                   <v-list-tile-title class="text--primary">{{post.userId.username}}</v-list-tile-title>
-                  <v-list-tile-sub-title class="font-weight-thin">Added {{post.createdDate}}</v-list-tile-sub-title>
+                  <v-list-tile-sub-title class="font-weight-thin">Added {{formatCreatedDate(post.createdDate)}}</v-list-tile-sub-title>
                 </v-list-tile-content>
 
                 <v-list-tile-action>
@@ -59,6 +59,7 @@
 
 <script>
   import { INFINITE_SCROLL_POSTS } from "../../queries";
+  import moment from 'moment';
 
   const pageSize = 2;
 
@@ -81,8 +82,11 @@
       }
     },
     methods: {
-      goToPost(_id){
-        this.$router.push(`/posts/${_id}`);
+      formatCreatedDate(date){
+        return moment(new Date(date)).format('ll');
+      },
+      goToPost(_id) {
+        this.$router.push(`/posts/${ _id }`);
       },
       showMorePosts() {
         this.pageNum += 1;
